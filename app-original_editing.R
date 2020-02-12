b3b8e66383297dbea7fc33979d59e198cc975378
devalc <deva4998@vandals.uidaho.edu>
2020-02-12 12:47:12 -0800

    new shiny commit. Updated plotly heatmap but still need to figure out plotly pie

--- app-original_editing.R
+++ app-original_editing.R
@@ -7,7 +7,7 @@ library(shiny)
 library(tidyverse)
 library(shinythemes)
 # library(gganimate)
-# library(plotly)
+library(plotly)
 
 # Data Preparation Steps
 
@@ -90,13 +90,14 @@ ui <- navbarPage("viz-WEPP",
                  selectInput(inputId="Watershed",label="Choose Watershed",choices = unique_watsheds,
                              selected = unique_watsheds[1],multiple = F),
                  
-                 selectInput(inputId="Scenario",label="Choose Scenario",choices = unique_scenario,
-                             selected = unique_scenario[1],multiple = T),
+                 # selectInput(inputId="Scenario",label="Choose Scenario",choices = unique_scenario,
+                 #             selected = unique_scenario[1],multiple = T),
                  
-                 # radioButtons(inputId = "border1",label = "Select Border",choices = c("Black"="#000000","White"="#ffffff")),
+                 radioButtons(inputId = "ScenVvar",label = "Select heatmap or specific variable",
+                              choices = c("Heatmap"="Heatmap","Pie Chart"="Pie Chart"), selected = "Heatmap")
                  
-                 selectInput(inputId="wshed_var",label="Choose Variable",choices = as.character(unique(colnames(data_wshed)))[4:19],
-                             selected = as.character(unique(colnames(data_wshed)))[8], multiple = F)
+                 # selectInput(inputId="wshed_var",label="Choose Variable",choices = as.character(unique(colnames(data_wshed)))[4:19],
+                 #             selected = as.character(unique(colnames(data_wshed)))[8], multiple = F)
                  
                  
              ),
@@ -104,15 +105,12 @@ ui <- navbarPage("viz-WEPP",
              # Main panel for displaying outputs ----
              mainPanel(
                  
-                 fluidRow(
-                     column(6, plotOutput("Plot5")),
-                     column(6, plotOutput("Plot6"))
-                 ),
-                 fluidRow(
-                     column(6, plotOutput("Plot7")),
-                     column(6, plotOutput("Plot8"))
-                 )
-             )),
+                 fluidPage(
+                 plotlyOutput("Plot5" ,height = "800px", width ="1200px")
+                 # plotOutput("Plot5",height = "800px", width ="800px" )
+                     
+                 ))
+             ),
     
     tabPanel("Spatial-Viz",
              sidebarPanel(
@@ -155,14 +153,18 @@ server <- function(input, output){
     
 
     hill_subset <- reactive({
-        filter(hill, Watershed %in% input$Watershed) 
+        dplyr::filter(hill, Watershed %in% input$Watershed) 
     })
     
     chn_subset <- reactive({
-        filter(data_chn, Watershed %in% input$Watershed) 
+        dplyr::filter(data_chn, Watershed %in% input$Watershed) 
     })
     
+    # wshed_subet <- reactive({
+    #     dplyr::filter(data_wshed, Watershed %in% input$Watershed) 
+    # })
     
+#################   HILLSLOPE DF #################      
     hill_arr_by_var <- reactive({
         hill_subset() %>% group_by(Scenario) %>% arrange_at(input$var1, desc) %>%
                 mutate(cumPercArea = cumsum(Hillslope.Area..ha.)/sum(Hillslope.Area..ha.)*100,
@@ -186,6 +188,8 @@ server <- function(input, output){
                 ) %>%
                 ungroup()})
     
+#################   CHANNEL DF #################  
+    
     chn_arr_by_var <- reactive({
         chn_subset() %>% group_by(Scenario) %>% arrange_at(input$chan_var, desc) %>%
             mutate(cumPercChanArea = cumsum(Channel.Area..ha.)/sum(Channel.Area..ha.)*100,
@@ -200,8 +204,57 @@ server <- function(input, output){
                    cumParticulateP.kg.ha. = cumsum(Particulate.P..kg.ha.)/sum(Particulate.P..kg.ha.)*100,
                    cumTotalP.kg.ha. = cumsum(Total.P..kg.ha.)/sum(Total.P..kg.ha.)*100) %>%
             ungroup()})
-    
-    
+
+#################   CHANNEL PLOTS #################     
+
+    output$Plot5 <- renderPlotly({
+        
+        
+        wshed_subet <- dplyr::filter(data_wshed, Watershed %in% input$Watershed)
+        
+        if (input$ScenVvar == "Heatmap") {
+        d <-  wshed_subet[,c(2,7:20)] %>% dplyr::mutate_if(is.numeric, scale)
+        
+        d.m <- reshape2::melt(d)
+        
+        
+        a<-ggplot(d.m, aes(Scenario, variable,  fill= value)) + 
+            geom_tile(inherit.aes = TRUE)  +
+            scale_fill_distiller(palette = "Spectral") + 
+            theme(
+                axis.text.x = element_text(angle = 90,colour = "Black", size = 12, face = "bold"),
+                axis.text.y = element_text(colour = "Black", size = 12, face = 'bold'),
+                axis.title = element_blank()
+                
+            )
+        a}else
+            if (input$ScenVvar == "Pie Chart") {
+                
+                d <-  wshed_subet[,c(2,7:20)] 
+                
+                d.m <- reshape2::melt(d)
+                
+                d.m <- d.m %>%
+                    group_by(variable) %>%
+                    mutate(total = sum(value),
+                           share = (value/total)*100) %>%
+                    ungroup() 
+                
+                ggplot(d.m) +
+                    facet_wrap(~variable) +
+                    geom_bar(aes(y = share, x = "", fill = Scenario), stat = "identity") +
+                    theme(
+                        axis.title = element_blank() 
+                    ) +
+                    coord_polar("y", start = 0) 
+                    
+                
+                }
+        # plotly::ggplotly(a)
+    })
+        
+        
+#################   CHANNEL PLOTS #################      
     output$Plot1 <- renderPlot({
         p1 <- chn_arr_by_var() %>% ggplot(aes(x= cumPercContriChanArea))
         if(input$chan_var ==  "Discharge..mm."){
@@ -302,7 +355,7 @@ server <- function(input, output){
         
     })
     
-
+#################   HILLSLOPE PLOTS #################       
     output$Plot_vs_CumArea <- renderPlot({
 
         p1 <- hill_arr_by_var()  %>% ggplot(aes(x=cumPercArea))
@@ -370,7 +423,7 @@ server <- function(input, output){
 
     })
 
-
+   
     output$Plot_vs_CumArea_abs <- renderPlot({
         
         p1 <- hill_arr_by_var()  %>% ggplot(aes(x=cumPercArea))
